2a921f169a8a7f7ba366401febc54e99370cb530
Toni Kolev <kolev@toni-kolev.com>
2017-06-06 18:17:41 +0300

    Solving some issues regarding autostart of blog, new issues available, working on them.

--- UI.Tests/BrowserHost.cs
+++ UI.Tests/BrowserHost.cs
@@ -5,11 +5,11 @@ namespace UITests
     public static class BrowserHost
     {
         public static readonly SelenoHost Instance = new SelenoHost();
-        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
+        public static readonly string RootUrl = @"http://localhost:60634/";
 
         static BrowserHost()
         {
-            Instance.Run("Blog", 60634);
+            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
             //RootUrl = Instance.Application.Browser.Url;
         }
 
