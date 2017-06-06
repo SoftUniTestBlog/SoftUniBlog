dac75777a327e8f5ed2713c997c72cc437e882a9
Toni Kolev <kolev@toni-kolev.com>
2017-06-06 17:44:53 +0300

    Setting up autorun of Blog for UI tests

---
+++ UI.Tests/BrowserHost.cs
@@ -0,0 +1,17 @@
+﻿using TestStack.Seleno.Configuration;
+
+namespace UITests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
+
+        static BrowserHost()
+        {
+            Instance.Run("Blog", 60634);
+            //RootUrl = Instance.Application.Browser.Url;
+        }
+
+    }
+}
