d23f8182e6677f00f6a795089a8c78b8c6ed8c29
Toni Kolev <kolev@toni-kolev.com>
2017-06-07 19:07:40 +0300

    Changing ports for SelenoHost to 60640

--- BasicFunctionalityTests/BrowserHost.cs
+++ BasicFunctionalityTests/BrowserHost.cs
@@ -6,12 +6,12 @@ namespace BasicFunctionalityTests
     public static class BrowserHost
     {
         public static readonly SelenoHost Instance = new SelenoHost();
-        public static readonly string RootUrl = "http://localhost:60634/Article/List";
+        public static readonly string RootUrl = "http://localhost:60640/Article/List";
 
         static BrowserHost()
         {
-            //Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
-            Instance.Run("Blog", 60634);
+            //Instance.Run("Blog", 60640, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
+            Instance.Run("Blog", 60640);
             //RootUrl = Instance.Application.Browser.Url;
         }
 
