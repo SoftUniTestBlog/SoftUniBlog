0672e4e14904a3a8b00bf9f88fa5d7f46b4c4109
Unknown <gtarkalanov@propeople.dk>
2017-06-07 12:46:05 +0300

    Auto stash before merge of "master" and "origin/master"

--- UI.Tests/BrowserHost.cs
+++ UI.Tests/BrowserHost.cs
@@ -1,18 +1,18 @@
-﻿using TestStack.Seleno.Configuration;
-
-namespace UITests
-{
-    public static class BrowserHost
-    {
-        public static readonly SelenoHost Instance = new SelenoHost();
-        public static readonly string RootUrl = "http://localhost:60639/";
-
-        static BrowserHost()
-        {
-            //Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
-            Instance.Run("..\\Blog", 60639);
-            //RootUrl = Instance.Application.Browser.Url;
-        }
-
-    }
-}
+﻿using TestStack.Seleno.Configuration;
+
+namespace UITests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = "http://localhost:60639/";
+
+        static BrowserHost()
+        {
+            //Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
+            Instance.Run("..\\Blog", 60639);
+            //RootUrl = Instance.Application.Browser.Url;
+        }
+
+    }
+}
